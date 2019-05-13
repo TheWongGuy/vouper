903c45c7a5198e90046e7e87d423c768f3be8c29
Adam Wong <adam@adamwong.me>
2019-05-12 20:33:21 -0400

    Added room-create component

--- vouper-frontend/src/app/app.module.ts
+++ vouper-frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RoomCreateComponent } from './rooms/room-create/room-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomCreateComponent
   ],
   imports: [
     BrowserModule
---
+++ vouper-frontend/src/app/rooms/room-create/room-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-room-create',
+    templateUrl: './room-create.component.html'
+})
+export class RoomCreateComponent {
+    onCreateRoom(){
+
+    }
+}
