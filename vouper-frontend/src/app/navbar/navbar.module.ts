c13d98e6aa3c646a1d25058a71f09fa9dea94e18
Adam Wong <adam@adamwong.me>
2019-06-05 12:01:33 -0400

    Created navbar component and module

---
+++ vouper-frontend/src/app/navbar/navbar.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar.component';
+
+import { MatToolbarModule } from '@angular/material';
+
+/**
+ * `NavbarModule`
+ * The navigation bar appended to the top of every page in Vouper.
+ * Accessible through `<app-navbar>`
+ */
+@NgModule({
+  declarations: [NavbarComponent],
+  imports: [
+    CommonModule,
+
+    MatToolbarModule
+  ],
+  exports: [
+    NavbarComponent
+  ]
+})
+export class NavbarModule { }
